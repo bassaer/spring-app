93fd51d710b65c9b76addfa2c830d614f93e3a42
Tsubasa Nakayama <app.nakayama@gmail.com>
2020-05-19 01:14:56 +0900

    add thymeleaf

--- src/main/java/com/github/bassaer/springapp/controllers/MainController.java
+++ src/main/java/com/github/bassaer/springapp/controllers/MainController.java
@@ -4,11 +4,13 @@ package com.github.bassaer.springapp.controllers;
 import com.github.bassaer.springapp.models.User;
 import com.github.bassaer.springapp.models.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @Autowired
@@ -18,6 +20,12 @@ public class MainController {
     @GetMapping
     public String index() {
         return "OK";
+    }
+
+    @GetMapping("/table")
+    public String table(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "table";
     }
 
     @GetMapping(path = "/all")
